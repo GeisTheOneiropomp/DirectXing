8d6f82319850aad5074194eb227d798acffdeb0a
LAPTOP-TS3NINN8\81902 <ikejose0@gmail.com>
2022-06-25 15:08:42 +0900

    Silly shader

--- Box/Shaders/default.hlsl
+++ Box/Shaders/default.hlsl
@@ -120,8 +120,7 @@ float4 PS(VertexOut pin) : SV_Target
     float3 r = reflect(-toEyeW, bumpedNormalW);
     float4 reflectionColor = gCubeMap.Sample(gsamLinearWrap, r);
     float3 fresnelFactor = SchlickApprox(fresnelR0, bumpedNormalW, r);
-    //litColor.rgb += shininess * fresnelFactor * reflectionColor.rgb;
-    litColor.rgb = ambientAccess;
+    litColor.rgb += shininess * fresnelFactor * reflectionColor.rgb;
     // Common convention to take alpha from diffuse albedo.
     litColor.a = diffuseAlbedo.a;
 
